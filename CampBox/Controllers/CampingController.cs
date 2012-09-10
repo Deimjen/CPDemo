666785d9f7780f1c277bf6e861e728cc899f0642
Deimjen <deimantas.syvokas@outlook.com>
2012-09-10 22:35:30 +0300

    vs2012 init

---
+++ CampBox/Controllers/CampingController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CampBox.Data.Implementations;
+using CampBox.Data.Models;
+
+namespace CampBox.Controllers
+{
+    public class CampingController : ApiController
+    {
+        // GET api/camping
+        public IEnumerable<Camp> Get()
+        {
+            var repo = new CampRepository();
+            return repo.Get();
+        }
+
+        // GET api/camping/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/camping
+        public void Post([FromUri]string value)
+        {
+            var repo = new CampRepository();
+            repo.Insert(new Camp { Title = value });
+        }
+
+        // PUT api/camping/5
+        public void Put(int id, [FromUri]string value)
+        {
+        }
+
+        // DELETE api/camping/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
